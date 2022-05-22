2d76b8a133946077546e2a119d85dbca696ae9b6
Torres <torresmthiago@gmail.com>
2022-05-22 15:56:12 -0300

    [Arkanoid] Ball speed limit
    Some collisions with corners added a lot of force to the ball object. Added the AdjustSpeed function

--- Arkanoid/Assets/Script/BallBehaviour.cs
+++ Arkanoid/Assets/Script/BallBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,61 @@ public class BallBehaviour : MonoBehaviour
 {
 
     private Rigidbody2D rigidbody;
-    public Vector2 velocity;
+    private Vector2 currentSpeed;
+
+    public Vector2 ballSpeed;
+    public GameObject playerPallet;
+    public Vector2 speedLimit;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.AddForce(velocity);
+        playerPallet = GameObject.FindWithTag("Player");
     }
+
+    private void FixedUpdate()
+    {
+        /*
+            The 'proper' way would be to apply a force in the opposite direction of the 
+            rigidbody's velocity. The amount of force should be proportional to the extent 
+            to which the rigidbody is exceeding its speed limit.
+        */
+
+        currentSpeed = new Vector2(rigidbody.velocity.x, rigidbody.velocity.y);
+        AdjustSpeed(currentSpeed);
+    }
+
+    private void AdjustSpeed(Vector2 currentSpeed)
+    {
+        int isXPositive = Mathf.Sign(currentSpeed.x) == 1 ? -1 : 1;
+        int isYPositive = Mathf.Sign(currentSpeed.y) == 1 ? -1 : 1;
+
+        if (Mathf.Abs(currentSpeed.x) > speedLimit.x)
+        {
+            float speedDiference = (Mathf.Abs(currentSpeed.x) - speedLimit.x) * isXPositive;
+            rigidbody.AddForce(new Vector2(speedDiference, 0));
+        }
+
+        if (Mathf.Abs(currentSpeed.y) > speedLimit.y)
+        {
+            float speedDiference = (Mathf.Abs(currentSpeed.y) - speedLimit.y) * isYPositive;
+            Debug.Log(speedDiference);
+            rigidbody.AddForce(new Vector2(0, speedDiference));
+        }
+    }
+
+    private void Update()
+    {
+        if (rigidbody.velocity == Vector2.zero && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(ballSpeed);
+        }
+        else if (rigidbody.velocity == Vector2.zero)
+        {
+            transform.position = new Vector3(playerPallet.transform.position.x, transform.position.y, 0);
+        }
+    }
+
+
 }
+
