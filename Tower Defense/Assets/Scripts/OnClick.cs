0ab3a93b2b6bc6a081fad263eec503e512a7db32
Thiago Torres <TorresmThiago@gmail.com>
2022-07-01 21:26:43 -0300

    [TD] Raycast2D for mouseInput

--- Tower Defense/Assets/Scripts/OnClick.cs
+++ Tower Defense/Assets/Scripts/OnClick.cs
@@ -7,15 +7,38 @@ using UnityEngine.Events;
 public class OnClick : MonoBehaviour
 {
     public UnityEvent unityEvent;
+    public string targetTag;
 
     void Awake()
     {
         if (unityEvent == null)
             unityEvent = new UnityEvent();
+
+        Debug.Log(targetTag);
+
     }
 
-    public void OnMouseDown()
+    private void Update()
     {
-        unityEvent.Invoke();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 cubeRay = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D cubeHit = Physics2D.Raycast(cubeRay, Vector2.zero);
+
+            if (cubeHit)
+            {
+                Debug.Log("Click Detected");
+                // whatever tag you are looking for on your game object
+                if (cubeHit.collider.tag == targetTag)
+                {
+                    unityEvent.Invoke();
+                }
+            }
+        }
     }
+
+    // public void OnMouseDown()
+    // {
+    //     unityEvent.Invoke();
+    // }
 }
