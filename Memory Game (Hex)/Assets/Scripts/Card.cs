0bba48c6033456b6ea2f365b97fb348716f65a51
Thiago Torres <TorresmThiago@gmail.com>
2022-07-08 19:53:00 -0300

    [MG] Card Asset Flip

---
+++ Memory Game (Hex)/Assets/Scripts/Card.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Card : MonoBehaviour
+{
+
+    public Sprite cardBack;
+
+
+    [SerializeField]
+    private string hexCode;
+
+    private TextMeshProUGUI _text;
+    private Image _image;
+    private bool showing;
+
+    void Start()
+    {
+        _text = GetComponentInChildren<TextMeshProUGUI>();
+        _image = GetComponent<Image>();
+        showing = false;
+    }
+
+    public void TriggerCardAnimation()
+    {
+        if (showing)
+        {
+            Hide();
+        }
+        else { Show(); }
+
+        showing = !showing;
+    }
+
+    public void Show()
+    {
+        _text.text = hexCode;
+        if (ColorUtility.TryParseHtmlString(hexCode, out Color color)) { _image.color = color; }
+        _image.sprite = null;
+    }
+
+    public void Hide()
+    {
+        _text.text = "";
+        _image.color = new Color(1, 1, 1, 1);
+        _image.sprite = cardBack;
+    }
+
+}
