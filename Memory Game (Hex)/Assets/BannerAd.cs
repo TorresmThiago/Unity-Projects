e58a157f3b5ba104fc7e67de1b297c1d88e81d9d
Thiago Torres <TorresmThiago@gmail.com>
2022-07-13 21:13:04 -0300

    [MG] BannerAd Asset
    +Minor ui size fix

---
+++ Memory Game (Hex)/Assets/BannerAd.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAd : MonoBehaviour
+{
+    [SerializeField] BannerPosition _bannerPosition = BannerPosition.BOTTOM_CENTER;
+
+    [SerializeField] string _androidAdUnitId = "Banner_Android";
+    [SerializeField] string _iOSAdUnitId = "Banner_iOS";
+    string _adUnitId = null;
+
+    void Start()
+    {
+#if UNITY_IOS
+        _adUnitId = _iOSAdUnitId;
+#elif UNITY_ANDROID
+        _adUnitId = _androidAdUnitId;
+#endif
+
+        Advertisement.Banner.SetPosition(_bannerPosition);
+        LoadBanner();
+    }
+
+    public void LoadBanner()
+    {
+        BannerLoadOptions options = new BannerLoadOptions
+        {
+            loadCallback = OnBannerLoaded,
+            errorCallback = OnBannerError
+        };
+
+        Advertisement.Banner.Load(_adUnitId, options);
+    }
+
+    void OnBannerLoaded()
+    {
+        Debug.Log("Banner loaded");
+        Advertisement.Banner.Show(_adUnitId);
+    }
+
+    void OnBannerError(string message)
+    {
+        Debug.Log($"Banner Error: {message}");
+    }
+
+}
